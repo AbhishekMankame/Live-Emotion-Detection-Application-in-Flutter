4c8415ecd23c46f9bb917fe8f1c4bfec5c57b18e
AbhishekMankame <abhishekmankame962@gmail.com>
2022-11-08 20:26:23 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:camera/camera.dart';
+import 'package:emotion_detection/home.dart';
+import 'package:flutter/material.dart';
+
+List<CameraDescription>? cameras;
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
+  runApp(new MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(primaryColor: Colors.deepPurple),
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
